66de9ed7e4ab158b86811774ae801c01b3b153a2
pawarJai <pawarjayesh468@gmail.com>
2026-03-02 14:25:37 +0530

    working on CI CD pipeline

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -65,7 +65,7 @@ export default function Dashboard() {
     <div className="p-6 space-y-6  bg-white min-h-screen">
       <div>
         <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800">
-          Dashboardd
+          Dashboard
         </h1>
         <p className="text-sm sm:text-base text-gray-500">
           Track your business metrics and performance
